4afdedc9302b0d24776f4b07fd49627400adf513
Artem Trusov <tavikingwork@gmail.com>
2022-10-24 14:20:42 +0300

    lesson 12 / classwork 2

---
+++ lesson12_modules_unit_test/cw2/calculator.js
@@ -0,0 +1,5 @@
+export const getSquaredArray = (arr) => arr.map((prevNum) => prevNum * prevNum);
+
+export const getOddNumbers = (arr) => arr.filter((num) => num % 2 === 1);
+
+export default (a, b) => a + b;
