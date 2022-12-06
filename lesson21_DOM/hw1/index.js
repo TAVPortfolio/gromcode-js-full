93015aebeb1370c1aa3886bfb7811d72069ace8f
Artem Trusov <tavikingwork@gmail.com>
2022-12-06 11:52:49 +0200

    fixed homework

--- lesson21_DOM/hw1/index.js
+++ lesson21_DOM/hw1/index.js
@@ -3,9 +3,11 @@
 export const getTitleElement = () => {
   const titleElement = document.querySelector('.title');
   console.dir(titleElement);
+  return titleElement;
 };
 
 export const getInputElement = () => {
   const inputElement = document.querySelector('input');
   console.dir(inputElement);
+  return inputElement;
 };
