5883dccda20999fcdbab6eca02b47ffe9100ae2c
Artem Trusov <tavikingwork@gmail.com>
2022-12-06 11:50:47 +0200

    added classwork and homework lesson 21

---
+++ lesson21_DOM/hw1/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+export const getTitleElement = () => {
+  const titleElement = document.querySelector('.title');
+  console.dir(titleElement);
+};
+
+export const getInputElement = () => {
+  const inputElement = document.querySelector('input');
+  console.dir(inputElement);
+};
