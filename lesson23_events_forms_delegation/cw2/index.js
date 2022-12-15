a098005d0039821764aa2514440256cad2aacf08
Artem Trusov <tavikingwork@gmail.com>
2022-12-15 15:53:12 +0200

    lesson 23 / classwork 2

---
+++ lesson23_events_forms_delegation/cw2/index.js
@@ -0,0 +1,67 @@
+const arenaElem = document.querySelector('.arena');
+
+const generateNumbersRange = (from, to) => {
+  const result = [];
+  for (let i = from; i <= to; i++) {
+    result.push(i);
+  }
+  return result;
+};
+const getLineSeats = () =>
+  generateNumbersRange(1, 10)
+    .map(
+      (seatNumber) => `
+  <div 
+  class="sector__seat" 
+  data-seat-number="${seatNumber}"
+  ></div>
+  `
+    )
+    .join('');
+
+const getSectorLines = () => {
+  const seatsString = getLineSeats();
+  return generateNumbersRange(1, 10)
+    .map(
+      (lineNumber) => `
+  <div 
+  class="sector__line" 
+  data-line-number="${lineNumber}"
+  >${seatsString}</div>
+  `
+    )
+    .join('');
+};
+
+const renderArena = () => {
+  const linesStrings = getSectorLines();
+  const sectorsString = generateNumbersRange(1, 3)
+    .map(
+      (sectorNumber) => `
+  <div 
+  class="sector" 
+  data-sector-number="${sectorNumber}"
+  >${linesStrings}</div>
+`
+    )
+    .join('');
+
+  arenaElem.innerHTML = sectorsString;
+};
+
+const onSeatSelect = (e) => {
+  const selectedSeatElem = document.querySelector('.board__selected-seat');
+  const isSeat = e.target.classList.contains('sector__seat');
+  if (!isSeat) {
+    return;
+  }
+  const seatNumber = e.target.dataset.seatNumber;
+  const lineNumber = e.target.closest('.sector__line').dataset.lineNumber;
+  const sectorNumber = e.target.closest('.sector').dataset.sectorNumber;
+
+  selectedSeatElem.textContent = `S ${sectorNumber} - L ${lineNumber} - S ${seatNumber}`;
+};
+
+arenaElem.addEventListener('click', onSeatSelect);
+
+renderArena();
